4d857d450f1cf3e3637cb52ab1095d08ecb2fa02
TriniVial <maria.vial@uc.cl>
2022-05-01 21:27:15 -0400

    Codigos ECG, teemperatura listos

---
+++ codigo_online/Temperatura - DS18B20/tempubidots.ino
@@ -0,0 +1,60 @@
+/****************************************
+ * Include Libraries
+ ****************************************/
+#include "UbidotsESPMQTT.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+/****************************************
+ * Define Constants
+ ****************************************/
+#define TOKEN "BBFF-motC5hfJSVJcDQCyzKZUlnqvYwJJXG" // Your Ubidots TOKEN
+#define WIFINAME "RL2" //Your SSID
+#define WIFIPASS "RONDALAZEN1928" // Your Wifi Pass
+#define MQTTCLIENTNAME "mymqttclient" // Your MQTT Client Name, it must be unique so we recommend to choose a random ASCCI name
+#define Pin D3
+
+float temp;
+OneWire ourWire(Pin);
+DallasTemperature sensors(&ourWire);
+
+Ubidots client(TOKEN, MQTTCLIENTNAME);
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+/****************************************
+ * Main Functions
+ ****************************************/
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  client.wifiConnection(WIFINAME, WIFIPASS);
+  sensors.begin();
+  client.begin(callback);
+  }
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(!client.connected()){
+      client.reconnect();
+      }
+  
+  sensors.requestTemperatures();       //Prepare the sensor for reading
+  
+  temp = sensors.getTempCByIndex(0);
+  
+  Serial.print(sensors.getTempCByIndex(0)); //Read and print the temperature in Celsius
+  client.add("temperature", temp); //Insert your variable Labels and the value to be sent
+  delay(1000);
+  Serial.println("Grados Centigrados");
+  client.ubidotsPublish("control");
+  
+  client.loop();
+  }
